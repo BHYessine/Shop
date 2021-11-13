7642b95287638a0dc1f4ac0851a4a175436fd785
Yessine Ben Hlima <87872940+BHYessine@users.noreply.github.com>
2021-11-13 11:45:26 +0100

    Update NavBar.js

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,29 +1,46 @@
 import "../NavBar/NavBar.css";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { GiFruitBowl, GiShoppingCart } from "react-icons/gi";
+import { FaSearch } from "react-icons/fa";
 import { FaHome } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { search } from "../../Redux/Actions/actions";
 
 function NavBar() {
+  const [keyword, setKeyword] = useState("")
   const counter = useSelector((state) => state.Counter);
-
+  const dispatch = useDispatch()
   return (
     <div className="navBarContainer">
-      <Link to="/">
-        <div className="logo" title="Fresh Bio">
+      <Link to="/" className="logo" title="Fresh Bio">        
           <GiFruitBowl />
-          Fresh Bio
-        </div>
+          <p className="logoTitle">Fresh Bio</p>        
       </Link>
-      <Link to="/">
-        <div className="home" title="Home Page">
-          <FaHome />
-        </div>
+      <Link to="/" className="home" title="Home Page">        
+          <FaHome style={{width:"80px"}} />        
       </Link>
 
-      <Link to="/cart">
-        <div className="btn-cart">
+      <div className="search">
+        <input
+          type="text"
+          title="Serach..."
+          placeholder="Serach For Products..."
+          value={keyword}
+          onChange={(e)=>setKeyword(e.target.value)}
+        />
+        <button 
+        className="btn-search" 
+        title="Search"
+        onClick={()=> dispatch(search(keyword))}
+        >
+          Search
+          <FaSearch />
+        </button>
+      </div>
+
+      <Link to="/cart" className="btn-cart">
+        <div >
           <GiShoppingCart className="icon-cart" title="Cart" />
           <span className="counter">{counter}</span>
         </div>
@@ -33,4 +50,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
